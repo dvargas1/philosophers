76a55c0dc33182f9bc4468044191aa43157d922f
dvargas1 <daniel_v93@hotmail.com>
2022-12-14 09:19:42 -0300

    Start Error handler

--- main.c
+++ main.c
@@ -3,33 +3,107 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
-void* routine2(void* x)
+void help()
 {
-    *(int*)x += 10;
-    printf("TESTE MOSTRANDO OUTRO VALOR = %d\n", *(int*)x);
+    printf("DEU RUIM CHEFE");
+    exit(1);
 }
 
-void* routine(void* x)
+void help2()
 {
-    printf("Valor de X = %d\n", *(int*)x);
+    printf("DEU RUIM CHEFEeeeeeeeeeeeee");
+    exit(1);
 }
-int main()
+
+typedef struct s_philo
 {
-    int x = 0;
-    x++;
-    x++;
-    pthread_t t1;
-    printf("%d\n", x);
-    x++;
-    x++;
-    x++;
-    pthread_create(&t1, NULL, &routine2, (void*)&x);
-    printf("%d\n", x);
-    x++;
-    pthread_join(t1, NULL);
+    int philo_id;
+    int time_to_die;
+    int time_to_eat;
+    int time_to_sleep;
+    int many_eats;
+}   t_philo;
 
+int ft_isdigit(char c)
+{
+    if (c >= '0' && c <= '9')
+        return 1;
+    return(0);
+}
+int ft_littleatoi(char *s)
+{
+    int i;
+    long int nb;
 
+    nb = 0;
+    i = 0;
+    while(s[i] && ft_isdigit(s[i]))
+    {
+        nb *= 10;
+		nb += s[i] - '0';
+        i++;
+    }
+    if(nb > 2147483647)
+        help();
+    return((int)nb);
+}
+
+int ft_isallnum(char *s)
+{
+    int i;
+
+    i = 0;
+    while(s[i])
+    {
+        if(s[i] < '0' || s[i] > '9')
+            return(1);
+        i++;
+    }
+    return(0);
+
+}
+
+int ft_checkargs(int argc, char **argv)
+{
+    int i;
+
+    i = 1;
+    while(i < argc)
+    {
+        if(ft_isallnum(argv[i]) != 0)
+            help2();
+        i++;
+    }
+    return(0);
+}
+
+int ft_ignite(t_philo *philo, int argc, char **argv)
+{
+    if(ft_checkargs(argc, argv) != 0)
+        help();
+    philo->philo_id = ft_littleatoi(argv[1]);
+    philo-> time_to_die = ft_littleatoi(argv[2]);
+    philo-> time_to_eat = ft_littleatoi(argv[3]);
+    philo->time_to_sleep = ft_littleatoi(argv[4]);
+    philo-> many_eats = ft_littleatoi(argv[5]);
+    return (0);
+}
+
+int main(int argc, char **argv)
+{
+    t_philo aristoteles;
+
+    if(argc < 6)
+        help();
+    if(ft_ignite(&aristoteles, argc, argv) == 0);
+    {
+        printf("Hi, i'm the first Philosofer of this function\n");
+        printf("My ID is: %d\n", aristoteles.philo_id);
+        printf("I will die in: %dms\n", aristoteles.time_to_die);
+        printf("I will sleep for: %dms\n", aristoteles.time_to_sleep);
+        printf("I will eat for: %dms\n", aristoteles.time_to_eat);
+        printf("I will eat %d times\n", aristoteles.many_eats);
+    }
     return 0;
 
 }
